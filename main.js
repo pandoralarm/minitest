287e8641f8c26d48180296d190f956c5bc95d28d
Alan Go <55218308+pandoralarm@users.noreply.github.com>
2022-08-25 10:07:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function getResult() {
+  //clear to rerun
+  $("#hasil").empty();
+
+  //For loop sejumlah x
+  var xval = $('#xval').val(); 
+  for (let i = 1; i <= xval; i++) {
+    $("#hasil").append('<li>'+divcurrent(i)+'</li>');
+    console.log(i);
+  }
+}
+
+function divcurrent(val){
+  //Check modulo 3 dan 5
+  var result = "";
+  if (val%3 == 0){
+    result = result.concat("Tiga");
+  } 
+  
+  if (val%5 == 0){
+    result = result.concat("Lima");
+  }
+
+  if (result == "") {
+    result = val;
+  }
+
+  return result;
+}
